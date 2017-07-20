163322f0cf308a8418c7ddd16bf5f6d3371c3aa4
Hobin <hobinkang@live.com>
2017-07-20 00:55:42 -0400

    python

--- stock_lstm.py
+++ stock_lstm.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -14,30 +15,17 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
 from keras.layers.recurrent import LSTM
 
-
-def get_stock_data(stock_name, isTraining, normalized=0):
-    url = 'http://chart.finance.yahoo.com/table.csv?s=%s&a=11&b=15&c=2011&d=29&e=10&f=2016&g=d&ignore=.csv' % stock_name
-    col_names = ['Date','Open','High','Low','Close','Volume','Adj Close']
-    stocks = pd.read_csv(url, header=0, names=col_names) 
+#Retrieves stock data from alphavantage
+def get_stock_data(stock_name, api_key, output_size):
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=%s&apikey=%s&datatype=csv&outputsize=%s' % (stock_name, api_key, output_size)
+    #load csv data from url
+    stocks = pd.read_csv(url)
+    #load data to DataFrame
     df = pd.DataFrame(stocks)
-    #if not isTraining:
-       # df1 = pd.DataFrame({'Date': ["2017-04-26"],
-                   #       'Open': [312.37],
-                         # 'High': [314.50],
-                        #  'Low': [310.25],
-                        #  'Close': [0],
-                        #  'Volume': [0],
-                        #  'Adj Close': [0]},
-                        #  index= [0])
-        #frames = [df1, df]
-        #df = pd.concat(frames)
-    date_split = df['Date'].str.split('-').str
-    df['Year'], df['Month'], df['Day'] = date_split
-    df["Volume"] = df["Volume"] / 10000
-    date_store = df
- 
-    df.drop(df.columns[[0,3,5,6, 7,8,9]], axis=1, inplace=True)
-    return df, date_store
+    
+    return df
+
+#Function to seperate Training data and Testing data
 
 def load_data(stock, seq_len):
     amount_of_features = len(stock.columns)
@@ -60,84 +48,143 @@ def load_data(stock, seq_len):
 
     return [x_train, y_train, x_test, y_test]
 
+#LSTM
 
+def lstm_model(layers):
+    model = Sequential()
 
+    model.add(LSTM(
+        input_dim=layers[0],
+        output_dim=layers[1],
+        return_sequences=True))
+    model.add(Dropout(0.2))
 
-class stock_lstm():
+    model.add(LSTM(
+        layers[2],
+        return_sequences=False))
+    model.add(Dropout(0.2))
 
-	def __init__(self, layers):
-		self.layers = layers
+    model.add(Dense(
+        output_dim=layers[2]))
+    model.add(Activation("linear"))
 
-	def create_model(self, dropout):
-		d = 0.2
+    start = time.time()
+    model.compile(loss="mse", optimizer="rmsprop",metrics=['accuracy'])
+    print("Compilation Time : ", time.time() - start)
+    return model
 
-		#create Sequential object from keras
-		self = Sequential()
-		self.add(LSTM(128, input_shape=(self.layers[1], self.layers[0]), return_sequences=True))
-		self.add(Dropout(dropout))
-		self.add(LSTM(64, input_shape=(self.layers[1], self.layers[0]), return_sequences=False))
-		self.add(Dropout(dropout))
-		self.add(Dense(16,kernel_initializer='uniform',activation='relu'))        
-		self.add(Dense(1,kernel_initializer='uniform',activation='linear'))
-		self.compile(loss='mse',optimizer='rmsprop',metrics=['accuracy'])
+#seq2seq model
 
-		return self
-
-
-	def fit(self, x_train, y_train):
-		self.fit(
-    	x_train,
-    	y_train,
-    	batch_size=256,
-    	nb_epoch=500,
-    	validation_split=0.1,
-    	verbose=1)
-
-		return self
-
-	def Score(self):
-
-		trainScore = self.evaluate(x_train, y_train, verbose=0)
-		print('Train Score: %.2f MSE (%.2f RMSE)' % (trainScore[0], math.sqrt(trainScore[0])))
-		testScore = self.evaluate(x_test, y_test, verbose=0)
-		print('Test Score: %.2f MSE (%.2f RMSE)' % (testScore[0], math.sqrt(testScore[0])))
+def seq2seq(layers):
+        d = 0.2
+        model = Sequential()
+        model.add(LSTM(128, input_shape=(layers[1], layers[0]), return_sequences=True))
+        model.add(Dropout(d))
+        model.add(LSTM(64, input_shape=(layers[1], layers[0]), return_sequences=False))
+        model.add(Dropout(d))
+        model.add(Dense(16, activation='relu'))        
+        model.add(Dense(1, activation='linear'))
+        model.compile(loss='mse',optimizer='rmsprop',metrics=['accuracy'])
+        return model
 
 
 def main():
 
-	#select stock
-	stock_name = 'TSLA'
-	df, date_store = get_stock_data(stock_name, 0, True)
-	print(df.head(23))
+	#Get Data
 
-	#write csv
+	api_key = '5HSLKBUWSP7B6DOX'
+	stock_name = 'MSFT'
+
+	output_size = 'full'
+	df = get_stock_data(stock_name, api_key, output_size)
+
+	print(df.head(10))
+
+	print("The shape of dataframe is:")
+
+	print(df.shape)
+
+	#Feature selection and process
+
+	data = df.drop(df.columns[[0,3,5]], axis=1)
+
+	print(data.head(5))
+
+	#Data to csv
+
 	today = datetime.date.today()
-	fromile_name = stock_name+'_stock_%s.csv' % today
-	df.to_csv(file_name)
+
+	dir_path = os.path.dirname(os.path.realpath('stock_lstm.py'))
+	file_name = dir_path + "/data/" + stock_name +'_stock_%s.csv' % today
+	data.to_csv(file_name)
+
+	print(data)
+
+	#Feauture Scaling
+
+	data['high'] = data['high'] / 100
+	data['open'] = data['open'] / 100
+	data['close'] = data['close'] / 100
+	print(data.head(5))
+
+	#Separate Data
+
+	#Chosen the days when exchange is operating
+	seq_length = 22
+	X_train, y_train, X_test, y_test = load_data(data[::-1], seq_length)
+	print("X_train:", X_train.shape)
+	print("y_train:", y_train.shape)
+	print("X_test:", X_test.shape)
+	print("y_test:", y_test.shape)
+
+	# model = build_model([3,lag,1])
+	model = seq2seq([3, seq_length ,1])
+
+	#Fit model and train
+
+	model.fit(
+    X_train,
+    y_train,
+    batch_size=256,
+    nb_epoch=100,
+    validation_split=0.1,
+    verbose=1)
 
 
-	#feature scaling
-	df['High'] = df['High'] / 100
-	df['Open'] = df['Open'] / 100
-	df['Close'] = df['Close'] / 100
-	print(df.head(5))
+	#Score
 
+	trainScore = model.evaluate(X_train, y_train, verbose=0)
+	print('Train Score: %.2f MSE (%.2f RMSE)' % (trainScore[0], math.sqrt(trainScore[0])))
 
-	#load data to dataframe
+	testScore = model.evaluate(X_test, y_test, verbose=0)
+	print('Test Score: %.2f MSE (%.2f RMSE)' % (testScore[0], math.sqrt(testScore[0])))
+
+	#getPredictionData('MSFT',0)
 	window = 22
-	x_train, y_train, x_test, y_test = load_data(df[::-1], window)
-	print("X_train", x_train.shape)
-	print("y_train", y_train.shape)
-	print("X_test", x_test.shape)
-	print("y_test", y_test.shape)
+	diff=[]
+	ratio=[]
+	p = model.predict(X_test)
 
-	print(x_train)
+	for u in range(0, len(X_test)):
+		pr = p[u][0]
+		ratio.append((y_test[u]/pr)-1)
+		diff.append(abs(y_test[u]- pr))
+    	#print(u, y_test[u], pr, (y_test[u]/pr)-1, abs(y_test[u]- pr))
 
 
+	import matplotlib.pyplot as plt2
+	print("Expected value: ",(y_test[-1] * 100))
+	print("Last Input: ",(X_test[-1] * 100))
+	print("Last Prediction: ",(p[-1] * 100))
+	plt2.plot(p ,color='red', label='prediction')
+	plt2.plot(y_test ,color='blue', label='y_test')
 
+	#plt2.legend(loc='upper left')
+	plt2.show()
+
+	print("The Stock values have been plotted.")
+
+	sys.exit()
 
 if __name__ == '__main__':
 	main()
-
-
-
